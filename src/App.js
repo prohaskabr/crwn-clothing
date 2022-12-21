76d9594a7b1fa875bf97b7213fa9c6a988c4b85c
Thiago Prohaska <thiago.prohaska@gmail.com>
2022-12-21 12:17:03 +0100

    add nav component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.components";
+import Navigation from "./routes/navigation/navigation.component";
+
+
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />} >
+        <Route index element={<Home />} />
+      </Route>
+
     </Routes>
   );
 };
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import {ReactComponent as CrownLogo} from '../../assets/crown.svg'
+import './navigation.style.scss'
+
+const Navigation = () => {
+    return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container ">        
+        <CrownLogo className="logo" />
+        </Link>        
+        <div className="nav-links-container">
+            <Link className="nav-link" to='/shop'>Shop</Link>
+        </div>        
+      </div>
+      <Outlet />
+    </Fragment>
+    )
+  }
+
+  export default Navigation;
