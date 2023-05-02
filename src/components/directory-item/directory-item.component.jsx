2b17d1519e58dced324e158d06e3803c80bffe3d
Thiago Prohaska <thiago.prohaska@gmail.com>
2023-05-02 11:20:33 +0200

    refactor directory item

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,16 +1,26 @@
-import { Link } from 'react-router-dom';
-import { BackgroundImage, Body, ItemContainer } from './directory-item.style';
+import { useNavigate } from 'react-router-dom';
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer,
+} from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
   const { title, imageUrl } = category;
+  const navigate = useNavigate();
+
+  const clickHandler = (route) => {
+    navigate(route);
+  };
+
   return (
-    <ItemContainer>
+    <DirectoryItemContainer onClick={() => clickHandler(`shop/${title}`)}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
-        <Link className='title' to={`shop/${title}`}> <p>Shop now</p></Link>
+        <p>Shop now</p>
       </Body>
-    </ItemContainer>
+    </DirectoryItemContainer>
   )
 }
 
--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -1,5 +1,4 @@
-import styled from "styled-components";
-
+import styled from 'styled-components';
 
 export const BackgroundImage = styled.div`
   width: 100%;
@@ -7,7 +6,7 @@ export const BackgroundImage = styled.div`
   background-size: cover;
   background-position: center;
   background-image: ${({ imageUrl }) => `url(${imageUrl})`};
-`
+`;
 
 export const Body = styled.div`
   height: 90px;
@@ -20,7 +19,6 @@ export const Body = styled.div`
   background-color: white;
   opacity: 0.7;
   position: absolute;
-  
   h2 {
     font-weight: bold;
     margin: 0 6px 0;
@@ -28,15 +26,14 @@ export const Body = styled.div`
     color: #4a4a4a;
     text-transform: uppercase;
   }
-  
   p {
     font-weight: lighter;
     font-size: 16px;
   }
-`
+`;
 
-export const ItemContainer = styled.div`
-  min-width: 30 %;
+export const DirectoryItemContainer = styled.div`
+  min-width: 30%;
   height: 240px;
   flex: 1 1 auto;
   display: flex;
@@ -45,24 +42,20 @@ export const ItemContainer = styled.div`
   border: 1px solid black;
   margin: 0 7.5px 15px;
   overflow: hidden;
-
+  &:first-child {
+    margin-right: 7.5px;
+  }
+  &:last-child {
+    margin-left: 7.5px;
+  }
   &:hover {
-    cursor: pointer;    
-    & ${BackgroundImage}{
+    cursor: pointer;
+    ${BackgroundImage} {
       transform: scale(1.1);
-      transition: transform 6s cubic - bezier(0.25, 0.45, 0.45, 0.95);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
     }
-    
-    & ${Body}{
+    ${Body} {
       opacity: 0.9;
     }
   }
-
-  &: first-child {
-    margin-right: 7.5px;
-  }
-    
-  &: last-child {
-    margin-left: 7.5px;
-  }
-`
+`;
