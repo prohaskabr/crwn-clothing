3a7c48d20c33a0e0ff3d64ffca7debe53c83b5f6
Thiago Prohaska <thiago.prohaska@gmail.com>
2023-05-15 09:47:14 +0200

    add logger middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,20 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+//import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+
+const logger = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action);
+    }
+
+    console.log('type:', action.type);
+    console.log('payload:', action.payload);
+    console.log('currentState:', store.getState());
+
+    next(action);
+
+    console.log('next state:', store.getState());
+};
 
 const middleWares = [logger];
 
